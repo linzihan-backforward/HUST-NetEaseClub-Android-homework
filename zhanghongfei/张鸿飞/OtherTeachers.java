b284d73e95bda174e888363378286c9bb51f1de7
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:35:46 +0800

    Update OtherTeachers.java

--- zhanghongfei/张鸿飞/OtherTeachers.java
+++ zhanghongfei/张鸿飞/OtherTeachers.java
@@ -1,9 +1,7 @@
 package com.zhanghongfei.homework;
 
 public class OtherTeachers {
-	public void printClassName() {
-		System.out.println("this.getClass().getClassName()");
-	}
+	public void printClassName();
 	public void function() {
 		System.out.println("其他老师教别的科目。");
 	}
