2ac28f3e35bcfde0b977dc10b0324f18ff21af02
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:36:47 +0800

    Update SchoolCounselor.java

--- zhanghongfei/张鸿飞/SchoolCounselor.java
+++ zhanghongfei/张鸿飞/SchoolCounselor.java
@@ -1,10 +1,7 @@
 package com.zhanghongfei.homework;
 
 public class SchoolCounselor extends Person {
-	public void printClassName() {
-		a=this.getClass().getClassName();
-		System.out.println(a);
-	}
+	public void printClassName();
 	public void function() {
 		System.out.println("辅导员管理学生。");
 	}
