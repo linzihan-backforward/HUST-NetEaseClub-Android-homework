dd13fe4cf46c1f74755618ee029551e5ef49df4f
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:35:25 +0800

    Update MathTeachers.java

--- zhanghongfei/张鸿飞/MathTeachers.java
+++ zhanghongfei/张鸿飞/MathTeachers.java
@@ -1,9 +1,7 @@
 package com.zhanghongfei.homework;
 
 public class MathTeachers extends Person implements AllTeachers{
-	public void printClassName() {
-		System.out.println("this.getClass().getClassName()");
-	}
+	public void printClassName()；
 	public void function() {
 		System.out.println("数学老师教数学。");
 	}
