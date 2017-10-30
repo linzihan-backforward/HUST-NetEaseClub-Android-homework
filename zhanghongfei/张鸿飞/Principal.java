34566fb98f144c083551dd89f5c7f11200ba44d3
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:36:33 +0800

    Update Principal.java

--- zhanghongfei/张鸿飞/Principal.java
+++ zhanghongfei/张鸿飞/Principal.java
@@ -1,10 +1,7 @@
 package com.zhanghongfei.homework;
 
 public class Principal extends Person {
-	public void printClassName() {
-		a=this.getClass().getClassName();
-		System.out.println(a);
-	}
+	public void printClassName();
 	public void function() {
 		System.out.println("校长管理整个学校");
 	}
