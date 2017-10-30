3aec79866cdae4bef48954a984ba9333d0328912
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:21:52 +0800

    Update Students.java

--- zhanghongfei/张鸿飞/Students.java
+++ zhanghongfei/张鸿飞/Students.java
@@ -2,7 +2,8 @@ package com.github.homework;
 
 public class Students extends Person {
 	public void printClassName() {
-		System.out.println("Students");
+		String a=this.getClass().getClassName();
+		System.out.println(a);
 	}
 	public void function() {
 		System.out.println("学生好好学习);
