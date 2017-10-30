74f61093af37222e14b967b529a8b4b46b7fab29
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:34:42 +0800

    Update ChineseTeachers.java

--- zhanghongfei/张鸿飞/ChineseTeachers.java
+++ zhanghongfei/张鸿飞/ChineseTeachers.java
@@ -1,9 +1,7 @@
 package com.zhanghongfei.homework;
 
 public class ChineseTeachers extends Personimplements AllTeachers{
-	public void printClassName() {
-		System.out.println("this.getClass().getClassName()");
-	}
+	public void printClassName();
 	public void function() {
 		System.out.println("语文老师教语文。");
 	}
