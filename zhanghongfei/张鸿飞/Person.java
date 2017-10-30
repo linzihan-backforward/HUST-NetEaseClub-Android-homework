e9c56a04d165994311ca10fe693638e11b742afd
emiyafei <32332232+emiyafei@users.noreply.github.com>
2017-10-30 20:20:25 +0800

    Update Person.java

--- zhanghongfei/张鸿飞/Person.java
+++ zhanghongfei/张鸿飞/Person.java
@@ -1,11 +1,12 @@
-package com.github.homework;
+package com.zhanghongfei.homework;
 
 public class Person {
 	public String name;
 	public int age;
 	public String sexuality;
 	public void printClassName() {
-		System.out.println("打印类名");
+		a=this.getClass().getClassName();
+		System.out.println("a");
 	}
 	public void function (){
 		System.out.println("功能");
